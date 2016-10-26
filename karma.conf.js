6451902be81f2c434a581462826163a2969cf6d5
unknown <art.frolkov@gmail.com>
2016-10-26 13:28:16 +0300

    change karma conf, delete empty file, add build/

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'buld/bundle.js',
+        'js/bundle.js',
         'vendor/js/angular-mocks.js',
         'js/tests/app-test.js'
     ],
